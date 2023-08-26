1d206b04a8946a99b49e90fccddd03c8ae38fc50
Llama <132833765+Redtropig@users.noreply.github.com>
2023-08-26 22:24:54 +0800

    Slice operation implemented.

--- server/src/models/FileReceiver.java
+++ server/src/models/FileReceiver.java
@@ -14,8 +14,8 @@ public class FileReceiver {
 
     private static final int FILE_TRANSFER_FRAGMENT_SIZE = 1024; // bytes
     private static final int FILE_TRANSFER_SERVER_PORT = 23333;
-    private static final File FOLDER_TO_SLICE = new File(".\\.tmp\\toSlice");
-    private static final File FOLDER_TO_INFER = new File(".\\.tmp\\toInfer");
+    protected static final File FOLDER_TO_SLICE = new File(".\\.tmp\\toSlice");
+    protected static final File FOLDER_TO_INFER = new File(".\\.tmp\\toInfer");
 
     private static Thread workingTask;
 
@@ -42,9 +42,16 @@ public class FileReceiver {
              Socket fileTransferSocket = fileServerSocket.accept();
              DataInputStream inputStream = new DataInputStream(fileTransferSocket.getInputStream()))
         {
-            FileUsage fileUsage = FileUsage.valueOf(inputStream.readUTF());
-            File destFile;
+            // receive FileUsage metadata
+            FileUsage fileUsage;
+            try {
+                fileUsage = FileUsage.valueOf(inputStream.readUTF());
+            } catch(IllegalArgumentException ex){
+                System.err.println("[ERROR] File Data Stream Received: Illegal Format.");
+                return;
+            }
 
+            File destFile;
             // FileUsage determines where to store the file
             switch (fileUsage) {
                 case TO_SLICE -> {
@@ -53,11 +60,11 @@ public class FileReceiver {
                 case TO_INFER -> {
                     destFile = new File(FOLDER_TO_INFER, inputStream.readUTF());
                 }
-                default -> {
-                    System.err.println("[ERROR] File Data Stream Received: Illegal Format.");
-                    return;
+                default -> { // should never reach
+                    destFile = null;
                 }
             }
+            destFile.deleteOnExit(); // register auto-deletion
 
             DataOutputStream fileOutStream = new DataOutputStream(new FileOutputStream(destFile));
 
--- server/src/models/InstructionReceiver.java
+++ server/src/models/InstructionReceiver.java
@@ -1,10 +1,17 @@
 package models;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+import static models.ExecutionAgent.*;
 
 /**
  * Instruction Receiver
@@ -14,7 +21,10 @@ import java.net.Socket;
  */
 public class InstructionReceiver {
     private static final int INSTRUCTION_TRANSFER_FRAGMENT_SIZE = 1024; // bytes
-    private static final ExecutionAgent executionAgent = ExecutionAgent.getExecutionAgent();
+    private static final File SLICING_OUT_DIR_DEFAULT = new File(SO_VITS_SVC_DIR + "\\dataset_raw");
+    private static final String[] AUDIO_FILE_EXTENSIONS_ACCEPTED = {"wav"};
+    private static final String AUDIO_FILE_EXTENSIONS_DESCRIPTION = "Wave File(s)(*.wav)";
+    private static final ExecutionAgent EXECUTION_AGENT = ExecutionAgent.getExecutionAgent();
 
     private static int port;
     private static Thread workingTask;
@@ -44,23 +54,86 @@ public class InstructionReceiver {
      * This will BLOCK the thread until the connection established.
      */
     private static void receive() {
-        try (ServerSocket serverSocket = new ServerSocket(InstructionReceiver.port);
-             Socket instructionSocket = serverSocket.accept();
-             DataInputStream inputStream = new DataInputStream(instructionSocket.getInputStream());
-             DataOutputStream outStream = new DataOutputStream(instructionSocket.getOutputStream()))
-        {
-            InstructionType instructionType = InstructionType.valueOf(inputStream.readUTF());
+        try (ServerSocket serverSocket = new ServerSocket(InstructionReceiver.port)) {
+            Socket instructionSocket = serverSocket.accept();
+            DataInputStream inputStream = new DataInputStream(instructionSocket.getInputStream());
+            PrintStream outputStream = new PrintStream(instructionSocket.getOutputStream(), true, StandardCharsets.UTF_8);
 
-            System.out.println("[INFO] " + instructionType.name() +
-                    " Instruction from: " + instructionSocket.getInetAddress() + ':' + instructionSocket.getPort());
+            // Read Instruction JSONString
+            String instructionJSONString = inputStream.readUTF();
 
             // InstructionType fetch
+            JSONObject instructionJSONObject;
+            InstructionType instructionType;
+            try {
+                // Parse to JSONObject
+                instructionJSONObject = new JSONObject(instructionJSONString);
+                instructionType = InstructionType.valueOf(instructionJSONObject.getString("INSTRUCTION"));
+            } catch (IllegalArgumentException | JSONException ex) {
+                System.err.println("[ERROR] Instruction Stream Received: Illegal Format.");
+                return;
+            }
+
+            System.out.println("[INFO] " + instructionType +
+                    " Instruction from: " + instructionSocket.getInetAddress() + ':' + instructionSocket.getPort());
+
             switch (instructionType) {
                 case CLEAR -> {
 
                 }
                 case SLICE -> {
+                    outputStream.println("[INFO] Slicing Audio(s)...");
+                    File[] voiceAudioFiles = FileReceiver.FOLDER_TO_SLICE.listFiles();
+                    // slice each voice file
+                    assert voiceAudioFiles != null;
+                    for (int i = voiceAudioFiles.length - 1; i >= 0; i--) {
+                        File voiceFile = voiceAudioFiles[i];
 
+                        // Command construction
+                        String[] command = {
+                                PYTHON_EXE.getAbsolutePath(),
+                                SLICER_PY.getAbsolutePath(),
+                                voiceFile.getPath(),
+                                "--out",
+                                SLICING_OUT_DIR_DEFAULT.getPath() + "\\" + instructionJSONObject.getString("spk"),
+                                "--min_interval",
+                                String.valueOf(instructionJSONObject.getInt("min_interval"))
+                        };
+
+                        // schedule a task
+                        int finalI = i;
+                        EXECUTION_AGENT.executeLater(
+                                command,
+                                null,
+                                (process) -> {
+                                    if (process.exitValue() == 0) {
+                                        outputStream.println("[INFO] Slicing completed: " + voiceFile.getName());
+                                    } else {
+                                        outputStream.println("[ERROR] \"" +
+                                                SLICER_PY.getName() +
+                                                "\" terminated unexpectedly, exit code: " +
+                                                process.exitValue()
+                                        );
+                                    }
+                                    // delete tmp voice file
+                                    voiceFile.delete();
+
+                                    if (finalI == 0) {
+                                        outputStream.println("[INFO] All Slicing Done.");
+
+                                        // enable related interactions after batch execution
+                                        try {
+                                            instructionSocket.close();
+                                        } catch (IOException e) {
+                                            throw new RuntimeException(e);
+                                        }
+                                    }
+                                }
+                        );
+                    }
+
+                    // execute ASAP
+                    EXECUTION_AGENT.invokeExecution();
                 }
                 case PREPROCESS -> {
 
@@ -72,11 +145,9 @@ public class InstructionReceiver {
 
                 }
                 default -> {
-                    System.err.println("[ERROR] Instruction Stream Received: Illegal Format.");
-                    return;
+
                 }
             }
-
 
 
         } catch (IOException e) {
